31f7254577103ad335928abea8b5d3b195101b10
santanagabi <gaboliveirasantana1@gmail.com>
2024-09-14 22:09:11 -0300

    implemented the pause button

--- app.js
+++ app.js
@@ -1,58 +1,69 @@
-const bells = new Audio('./sounds/bell.wav'); 
-const startBtn = document.querySelector('.btn-start'); 
-const resetBtn = document.querySelector('.btn-reset'); 
-const session = document.querySelector('.minutes'); 
-const seconds = document.querySelector('.seconds');
-let myInterval; 
+const bells = new Audio("./sounds/bell.wav");
+const startBtn = document.querySelector(".btn-start");
+const resetBtn = document.querySelector(".btn-reset");
+const pauseBtn = document.querySelector(".btn-pause");
+const session = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+let myInterval;
 let state = true;
+let isPaused = false;
+let totalSeconds; // guarda o tempo restante
 
 const appTimer = () => {
-  const sessionAmount = Number.parseInt(session.textContent)
+  const sessionAmount = Number.parseInt(session.textContent);
 
-  if(state) {
+  if (state) {
     state = false;
     let totalSeconds = sessionAmount * 60;
 
     const updateSeconds = () => {
-      const minuteDiv = document.querySelector('.minutes');
-      const secondDiv = document.querySelector('.seconds');
-    
-      totalSeconds--;
-    
-      let minutesLeft = Math.floor(totalSeconds/60);
-      let secondsLeft = totalSeconds % 60;
-    
-      if(secondsLeft < 10) {
-        secondDiv.textContent = '0' + secondsLeft;
-      } else {
-        secondDiv.textContent = secondsLeft;
+      if (!isPaused) {
+        totalSeconds--;
+
+        let minutesLeft = Math.floor(totalSeconds / 60);
+        let secondsLeft = totalSeconds % 60;
+
+        if (secondsLeft < 10) {
+          seconds.textContent = "0" + secondsLeft;
+        } else {
+          seconds.textContent = secondsLeft;
+        }
+        session.textContent = `${minutesLeft}`;
+
+        if (minutesLeft === 0 && secondsLeft === 0) {
+          bells.play();
+          clearInterval(myInterval);
+        }
       }
-      minuteDiv.textContent = `${minutesLeft}`
-    
-      if(minutesLeft === 0 && secondsLeft === 0) {
-        bells.play()
-        clearInterval(myInterval);
-      }
-    }
+    };
 
     myInterval = setInterval(updateSeconds, 1000);
   } else {
-    alert('Session has already started.')
+    alert("Session has already started.");
   }
-}
+};
 
 const resetTime = () => {
   clearInterval(myInterval);
-  session.textContent = '25';
-  seconds.textContent = '00';
+  session.textContent = "25";
+  seconds.textContent = "00";
   state = true;
-}
+  isPaused = false;
+};
 
+const togglePause = () => {
+  if (!state) {
+    isPaused = !isPaused;
+    pauseBtn.textContent = isPaused ? "Resume" : "Pause";
+  } else {
+    alert("You need to start the timer first.");
+  }
+};
 
-
-startBtn.addEventListener('click', appTimer);
+startBtn.addEventListener("click", appTimer);
 
 // TODO: Create a "Reset" button that pauses the timer and brings it back to its initial time.
-resetBtn.addEventListener('click', resetTime);
+resetBtn.addEventListener("click", resetTime);
 
 // TODO: Add a "Pause" button that stops the timer when pressed and resumes the timer when pressed again.
+pauseBtn.addEventListener("click", togglePause);
